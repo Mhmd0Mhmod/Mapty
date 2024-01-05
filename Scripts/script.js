89a92299f972a9d04d279dff8ce67152429db093
Mhmd0Mhmod <iammhmdmhmod@gmail.com>
2024-01-05 17:17:49 +0200

    update

--- Scripts/script.js
+++ Scripts/script.js
@@ -10,9 +10,23 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-navigator.geolocation?.getCurrentPosition(function(postion){
-    const {latitude,longitude}=postion.coords;
-    console.log(latitude,longitude);
-},function(){
+navigator.geolocation?.getCurrentPosition(
+  function (postion) {
+    const { latitude, longitude } = postion.coords;
+    console.log(latitude, longitude);
+    var map = L.map("map").setView([51.505, -0.09], 13);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker([51.5, -0.09])
+      .addTo(map)
+      .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+      .openPopup();
+  },
+  function () {
     alert("can't get ur loaction");
-})
+  }
+);
